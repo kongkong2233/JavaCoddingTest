843ec9c35cc863a333a5f6fdc50d52261f68af3d
Yubin-Ji <87416836+kongkong2233@users.noreply.github.com>
2024-11-29 10:30:50 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 12.86 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+        String answer = "";
+        
+        char[] over_char = overwrite_string.toCharArray();
+        
+        answer = my_string.substring(0, s) + 
+            overwrite_string + my_string.substring(s + over_char.length);
+            
+        return answer;
+    }
+    
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        
+        Scanner sc = new Scanner(System.in);
+        String my_string = sc.nextLine();
+        String overwrite_string = sc.nextLine();
+        int s = sc.nextInt();
+        
+        sc.nextLine();
+        
+        String result = sol.solution(my_string, overwrite_string, s);
+        System.out.print(result);
+    }
+}
