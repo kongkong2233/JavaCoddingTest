2351f03444b908383bca0f41bbd975e73d840af3
Yubin-Ji <87416836+kongkong2233@users.noreply.github.com>
2025-01-01 13:07:06 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 1.92 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        for (int i = 0; i < queries.length; i++) {
+            int a = queries[i][0];
+            int b = queries[i][1];
+            
+            for (int j = a; j <= b; j++) {
+                arr[j] += 1;
+            }
+        }
+        
+        int[] answer = arr.clone();
+        return answer;
+    }
+}
