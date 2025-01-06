ea3df041025cbf2fbef7ba12bcae0917feb179ed
Yubin-Ji <87416836+kongkong2233@users.noreply.github.com>
2025-01-06 11:40:19 +0900

    [level 0] Title: A 강조하기, Time: 0.07 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String myString) {
+        char[] ch = myString.toCharArray();
+        
+        for (int i = 0; i < ch.length; i++) {
+            if (ch[i] == 'a') {
+                ch[i] = Character.toUpperCase(ch[i]);
+            } else if (ch[i] != 'A') {
+                ch[i] = Character.toLowerCase(ch[i]);
+            }
+        }
+        
+        String answer = new String(ch);
+        return answer;
+    }
+}
