8a0b2e4bfeb82b9ebdb46cde8b359ba7e990f09d
Yubin-Ji <87416836+kongkong2233@users.noreply.github.com>
2025-02-06 11:31:22 +0900

    [level 0] Title: 모스부호 (1), Time: 13.74 ms, Memory: 91.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(String letter) {
+        String[] morse = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        
+        String answer = "";
+        String[] arr = letter.split(" ");
+        
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < morse.length; j++) {
+                if (arr[i].equals(morse[j])) {
+                    answer += (char)('a'+j);
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
