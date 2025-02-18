856468951efa49bf65ff279136b07ea07e6f187c
Yubin-Ji <87416836+kongkong2233@users.noreply.github.com>
2025-02-18 13:57:00 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.16 ms, Memory: 88.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(String my_string) {
+        String[] str = my_string.split(" ");
+        int answer = Integer.parseInt(str[0]);
+        
+        for (int i = 1; i < str.length; i += 2) {
+            int num = Integer.parseInt(str[i + 1]);
+            
+            if (str[i].equals("+")) {
+                answer += num;
+            } else if (str[i].equals("-")) {
+                answer -= num;
+            }
+        }
+        
+        return answer;
+    }
+}
