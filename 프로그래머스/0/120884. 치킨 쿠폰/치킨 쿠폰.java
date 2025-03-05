e12219f3b1d40edfbe8a0fe731ac95380c259ebf
Yubin-Ji <87416836+kongkong2233@users.noreply.github.com>
2025-03-05 21:56:44 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.02 ms, Memory: 91.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int chicken) {
+        int answer = chicken / 9;
+        
+        if (chicken > 1 && chicken % 9 == 0) {
+            answer--;
+        }
+        
+        return answer;
+    }
+}
