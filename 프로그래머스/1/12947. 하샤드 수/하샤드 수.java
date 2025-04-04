28278bf4f51093c6c75c9a0b95840d9051a7e8a2
Yubin-Ji <87416836+kongkong2233@users.noreply.github.com>
2025-04-04 10:51:32 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 86.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean solution(int x) {
+        int sum = 0;
+        int num = x;
+        
+        while (x != 0) {
+            sum += x % 10;
+            x /= 10;
+        }
+        
+        return num % sum == 0 ? true : false;
+    }
+}
