9bc545313e68bf426def699b536135f98fcd30ea
Yubin-Ji <87416836+kongkong2233@users.noreply.github.com>
2025-04-17 15:12:45 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 2.81 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -2,7 +2,7 @@ class Solution {
     public String solution(int[] food) {
         StringBuilder sb = new StringBuilder();
         
-        for (int i = 0; i < food.length; i++) {
+        for (int i = 1; i < food.length; i++) {
             int count = food[i] / 2;
             sb.append(String.valueOf(i).repeat(count));
         }
