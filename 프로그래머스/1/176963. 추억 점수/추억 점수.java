ac57bca795aecc406c3f266decbfd8b1b4cb5e23
Yubin-Ji <87416836+kongkong2233@users.noreply.github.com>
2025-04-25 14:09:49 +0900

    [level 1] Title: 추억 점수, Time: 1.63 ms, Memory: 93.2 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -1,13 +1,26 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
         int[] answer = new int[photo.length];
+        Map<String, Integer> map = new LinkedHashMap<>();
+        
+        for (int i = 0; i < name.length; i++) {
+            map.put(name[i], yearning[i]);
+        }
         
         for (int i = 0; i < photo.length; i++) {
-            for (int j = 0; j < photo[i].length; j++) {
-                for (int k = 0; k < name.length; k++) {
-                    if (photo[i][j].equals(name[k])) answer[i] += yearning[k];
+            String[] person = photo[i];
+            int score = 0;
+            
+            for (int j = 0; j < person.length; j++) {
+                String personName = person[j];
+                
+                if (map.containsKey(personName)) {
+                    score += map.get(personName);
                 }
             }
+            answer[i] = score;
         }
         
         return answer;
